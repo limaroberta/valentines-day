f2a3076219cc2716c6bb6d8d2d73c6785b50fa45
Roberta Azambuja <robertaazambuja01@gmail.com>
2022-05-25 15:09:05 -0300

    Desktop grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { itemData } from "./list";
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
 }
 
-export default App;
+export default function QuiltedImageList() {
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+
+  return (
+    <>
+      <ImageList
+        sx={{ width: "100%", height: "auto" }}
+        variant="quilted"
+        cols={`${getWindowDimensions().width > 1000 ? 10 : 4} `}
+        rowHeight={150}
+      >
+        {itemData.map((item) => (
+          <ImageListItem
+            key={item.img}
+            cols={item.cols || 1}
+            rows={item.rows || 1}
+          >
+            <img
+              {...srcset(item.img, 121, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
+  );
+}
---
+++ src/list.js
@@ -0,0 +1,85 @@
+import pic01 from "./assets/pic01.jpg";
+import pic02 from "./assets/pic02.jpg";
+import pic03 from "./assets/pic03.jpg";
+import pic04 from "./assets/pic04.jpg";
+import pic05 from "./assets/pic05.jpg";
+import pic06 from "./assets/pic06.jpg";
+import pic07 from "./assets/pic07.jpg";
+import pic08 from "./assets/pic08.jpg";
+import pic09 from "./assets/pic09.jpg";
+import pic10 from "./assets/pic10.jpg";
+import pic11 from "./assets/pic11.jpg";
+import gif01 from "./assets/gif01.gif";
+import gif02 from "./assets/gif02.gif";
+
+export const itemData = [
+  {
+    img: pic01,
+    title: "Foto 1",
+    rows: 4,
+    cols: 2,
+  },
+  {
+    img: pic02,
+    title: "Foto 2",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: gif01,
+    title: "Gif 1",
+    rows: 2,
+  },
+  {
+    img: pic03,
+    title: "Coffee",
+  },
+  {
+    img: pic04,
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: pic05,
+    title: "Honey",
+    rows: 3,
+    cols: 2,
+  },
+  {
+    img: pic06,
+    title: "Basketball",
+    cols: 2,
+    rows: 2,
+  },
+  {
+    img: pic09,
+    title: "Fern",
+  },
+  {
+    img: pic07,
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: pic10,
+    title: "Tomato basil",
+    rows: 2,
+  },
+  {
+    img: gif02,
+    title: "Sea star",
+    rows: 2,
+  },
+
+  {
+    img: pic08,
+    title: "Bike",
+    cols: 2,
+  },
+  {
+    img: pic11,
+    title: "Hats",
+    cols: 2,
+  },
+];
